5baf9f5f39df163223115ee749ba9ca9682f3ff5
Alexandr <crazy.top@mail.ru>
2022-09-14 12:24:24 +0300

    Complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void EvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0)
+            count++;
+    Console.WriteLine(count);
+}
+
+int[] arr = new int[6];
+FillArray(arr);
+Console.WriteLine(string.Join(", ", arr));
+EvenNumber(arr);
