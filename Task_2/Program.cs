6404138d7b2e043864b36a0a8ad7ccb35867f514
Alexandr <crazy.top@mail.ru>
2022-09-14 12:42:03 +0300

    Complete

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-99, 100);
+}
+
+int Summa(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        sum += array[i];
+    return sum;
+}
+
+int[] arr = new int[6];
+FillArray(arr);
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine(Summa(arr));
